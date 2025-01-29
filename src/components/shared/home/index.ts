09aafd665ae0e664cea5126e83edb324a7fc0ef6
Kakabay <2kakabayashyrberdyew@gmail.com>
2025-01-29 14:35:35 +0500

    build

--- src/components/shared/home/index.ts
+++ src/components/shared/home/index.ts
@@ -2,5 +2,4 @@ export { HomeHero } from "./home-hero";
 export { HomeTime } from "./home-time";
 export { HomeAbout } from "./home-about";
 export { HomeTheme } from "./home-theme";
-export { HomePartners } from "./home-partners";
 export { HomeOffers } from "./home-offers";
