ce24e44b0e5fe97cca6b4fb0d32acf894235ff0a
Kakabay <2kakabayashyrberdyew@gmail.com>
2025-01-30 18:09:28 +0500

    added build config

--- vite.config.ts
+++ vite.config.ts
@@ -9,4 +9,14 @@ export default defineConfig({
       "@": path.resolve(__dirname, "./src"),
     },
   },
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          "react-vendor": ["react", "react-dom", "react-router-dom"], // Вынос библиотеки React
+          "ui-library": ["@/components/shared"], // Отдельный чанк для UI-компонентов
+        },
+      },
+    },
+  },
 });
