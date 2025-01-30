1ef6a9f730dd5664d0c8fb9c26f6f626ae12b75f
Kakabay <2kakabayashyrberdyew@gmail.com>
2025-01-30 16:42:25 +0500

    build

--- src/components/shared/home/home-time.tsx
+++ src/components/shared/home/home-time.tsx
@@ -28,17 +28,17 @@ const contacts = [
   {
     title: "contact@turkmenexpo.com",
     subtitle: "Адрес электронной почты",
-    img: "/contacts/mail.svg",
+    img: "/contacts/home/mail.svg",
   },
   {
     title: "здание ТПП Туркменистана",
     subtitle: "Адрес выставки",
-    img: "/contacts/location.svg",
+    img: "/contacts/home/location.svg",
   },
   {
     title: "+99371871812; 99363719588",
     subtitle: "Контактный номер",
-    img: "/contacts/mobile.svg",
+    img: "/contacts/home/mobile.svg",
   },
 ];
 
