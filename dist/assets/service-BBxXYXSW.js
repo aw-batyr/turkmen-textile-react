6089f2c19ea7e939fbdee98f552fd1668bf6c4e7
Batyr <awbatyr@gmail.com>
2025-02-04 16:48:40 +0500

    build

--- dist/assets/service-BBxXYXSW.js
+++
@@ -1,1 +0,0 @@
-import{a as s}from"./index-Doh9iehP.js";const o="https://turkmentextile.turkmenexpo.com/app/api/v1",n=async t=>(await s.post(`${o}/book_stand_form`,t)).status===201,e=async t=>(await s.post(`${o}/become_sponsor_form`,t)).status===201,p=async t=>(await s.post(`${o}/contact_form`,t)).status===201;export{n as a,p as b,e as p};
