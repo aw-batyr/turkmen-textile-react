9e0e4ad19178cc235ff28f8540f710f0a6ec4d53
Kakabay <2kakabayashyrberdyew@gmail.com>
2025-02-09 18:36:07 +0500

    build

---
+++ dist/assets/service-D5qlX6-G.js
@@ -0,0 +1,1 @@
+import{a as s}from"./axios-upsvKRUO.js";const o="https://turkmentextile.turkmenexpo.com/app/api/v1",n=async t=>(await s.post(`${o}/book_stand_form`,t)).status===201,e=async t=>(await s.post(`${o}/become_sponsor_form`,t)).status===201,p=async t=>(await s.post(`${o}/contact_form`,t)).status===201;export{n as a,p as b,e as p};
---
+++ dist/assets/use-scroll-top-ByehxtTO.js
@@ -0,0 +1,1 @@
+import{a as r}from"./react-vendor-OnsDvTPE.js";const t=o=>{r.useEffect(()=>{window.scrollTo({behavior:"smooth",top:0})},[o])};export{t as u};
