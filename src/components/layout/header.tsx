2f1ade06175e4b399b0eb06c848a3e75e2f45033
Batyr <awbatyr@gmail.com>
2025-02-10 16:54:01 +0500

    change

--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -84,7 +84,7 @@ export const Header: FC = () => {
             </nav>
           </div>
 
-          <div className="flex items-center">
+          <div className="flex items-center gap-3">
             <LangMenu className="lg:hidden" />
             <Burger />
           </div>
--- src/components/shared/lang-menu.tsx
+++ src/components/shared/lang-menu.tsx
@@ -26,7 +26,9 @@ export const LangMenu: FC<Props> = ({ className }) => {
 
   return (
     <Popover open={isOpen} onOpenChange={() => setIsOpen(!isOpen)}>
-      <PopoverTrigger className={cn("flex items-center gap-2", className)}>
+      <PopoverTrigger
+        className={cn("flex text-white items-center gap-2", className)}
+      >
         <img src={lang === Language.RU ? "/ru.svg" : "/english.svg"} alt="" />
         {lang === Language.RU ? "Ру" : "En"}
         <svg
@@ -40,7 +42,7 @@ export const LangMenu: FC<Props> = ({ className }) => {
             fill-rule="evenodd"
             clip-rule="evenodd"
             d="M4.23021 8.01443C4.35919 7.70304 4.66305 7.5 5.00011 7.5H15.0001C15.3372 7.5 15.641 7.70304 15.77 8.01443C15.899 8.32583 15.8277 8.68426 15.5894 8.92259L10.5894 13.9226C10.2639 14.248 9.73629 14.248 9.41085 13.9226L4.41085 8.92259C4.17252 8.68426 4.10122 8.32583 4.23021 8.01443ZM7.01195 9.16667L10.0001 12.1548L12.9883 9.16667H7.01195Z"
-            fill="#2C2828"
+            fill="#fff"
           />
         </svg>
       </PopoverTrigger>
@@ -55,7 +57,7 @@ export const LangMenu: FC<Props> = ({ className }) => {
                 setIsOpen(false);
               }}
               key={i}
-              className="flex gap-3 py-1 items-center cursor-pointer"
+              className="flex gap-3 py-1  items-center cursor-pointer"
             >
               <img
                 src={item.lang === Language.RU ? "/ru.svg" : "/english.svg"}
