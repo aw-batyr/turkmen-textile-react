c9dc77ac6c64979a95fb6268705c68dac5d299a5
Batyr <awbatyr@gmail.com>
2025-05-24 15:51:46 +0500

    removed gap-20

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -11,7 +11,7 @@ import {
 
 export default function Home() {
   return (
-    <div className="flex flex-col gap-20">
+    <div className="flex flex-col">
       <HomeHero />
       <HomeActions />
       <HomeAbout />
