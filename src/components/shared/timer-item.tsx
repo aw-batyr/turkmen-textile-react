3ba6a43418436220786d8dcfaf8f6ac81c3d8448
Batyr <awbatyr@gmail.com>
2025-05-26 17:20:02 +0500

    changed colors from and to

--- src/components/shared/timer-item.tsx
+++ src/components/shared/timer-item.tsx
@@ -10,14 +10,14 @@ export const TimerItem = ({
   <div className="timer-item flex flex-col items-center">
     <div className="relative sm:h-14 h-6 w-16 overflow-hidden font-bold">
       <span
-        className={`absolute left-1/2 -translate-x-1/2 black bg-gradient-to-t from-[#F38835] to-[#F1700D] text-transparent bg-clip-text sm:text-5xl text-xl leading-none transition-all duration-300 ${
+        className={`absolute left-1/2 -translate-x-1/2 black bg-gradient-to-t from-[#F1700D] to-[#F38835] text-transparent bg-clip-text sm:text-5xl text-xl leading-none transition-all duration-300 ${
           prevValue !== value ? " opacity-0" : " opacity-100"
         }`}
       >
         {prevValue}
       </span>
       <span
-        className={`absolute left-1/2 -translate-x-1/2 black bg-gradient-to-t from-[#F38835] to-[#F1700D] text-transparent bg-clip-text  sm:text-5xl text-xl leading-none transition-all duration-300 ${
+        className={`absolute left-1/2 -translate-x-1/2 black bg-gradient-to-t from-[#F1700D] to-[#F38835] text-transparent bg-clip-text  sm:text-5xl text-xl leading-none transition-all duration-300 ${
           prevValue !== value ? "opacity-100" : "opacity-0"
         }`}
       >
