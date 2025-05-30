512c8c933bb29aa8b25e057810c1239ffcec5e30
Kakabay <2kakabayashyrberdyew@gmail.com>
2025-05-30 06:12:29 +0500

    changes

---
+++ src/locales/en/comment.ts
@@ -0,0 +1,29 @@
+// {
+//     "text": "Fashion shows",
+//     "hover": true,
+//     "hoverItems": [
+//       {
+//         "text": "Foreign designers",
+//         "link": "/impressions"
+//       },
+//       {
+//         "text": "Turkmen designers",
+//         "link": ""
+//       }
+//     ]
+//   }
+
+// {
+//     "text": "Master classes",
+//     "hover": true,
+//     "hoverItems": [
+//       {
+//         "text": "For designers",
+//         "link": "/impressions"
+//       },
+//       {
+//         "text": "For visitors",
+//         "link": ""
+//       }
+//     ]
+//   }
